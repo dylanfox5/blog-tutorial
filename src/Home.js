b7018929448032492094feabd68134f37d6116eb
Dylan Fox <dylan.fox.kc@gmail.com>
2022-06-26 20:45:12 -0500

    learning useState

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // let name = "mario";
+    const [name, setName] = useState("mario");
+    const [age, setAge] = useState(23);
 
     const handleClick = (e)  => {
-        console.log("Hello, ninjas", e);
-    }
-
-    const handleClickAgain = (name) => {
-        console.log("Hello", name);
+        setName("luigi");
+        setAge(30);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={() => handleClickAgain("mario")}>Click me again</button>
         </div>
     );
 }
