5a2d502e155d387d9807773a846cb6f369069c8e
Dylan Fox <dylan.fox.kc@gmail.com>
2022-06-26 20:34:46 -0500

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e)  => {
+        console.log("Hello, ninjas", e);
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("Hello", name);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => handleClickAgain("mario")}>Click me again</button>
         </div>
     );
 }
